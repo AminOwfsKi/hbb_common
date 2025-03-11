2fd0343aec78753bec2556027b4a04ba9f72610f
AminOwfsKi <76923921+AminOwfsKi@users.noreply.github.com>
2025-03-12 00:25:46 +0330

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.parsinaco.com"];
+pub const RS_PUB_KEY: &str = "eAutz2ETGPBcrsrUsu5F4E8K2B1IfcxI4MaE+7K3P94=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
